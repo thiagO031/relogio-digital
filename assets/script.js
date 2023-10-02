08c1660b8c043d272a9a40aa6937bed38e97b4c1
Thiago Torres Santana <thiago.santana@setrab.df.gov.br>
2023-10-02 12:15:16 -0300

    Status: Adicionado interação no projeto

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,18 @@
-let horas = document.querySelector("#horas")
-let minutos = document.querySelector("#minutos")
-let segundos = document.querySelector("#segundos")
+let horas = document.querySelector("#horas");
+let minutos = document.querySelector("#minutos");
+let segundos = document.querySelector("#segundos");
+let body = document.querySelector(".body");
 
 const relogio = setInterval(function time() {
+    
+    // configuração das horas do relogio
     let dateToday = new Date();
     let hr = dateToday.getHours();
     let min = dateToday.getMinutes();
     let s = dateToday.getSeconds();
 
     if (hr < 10) hr = '0' + hr;
-
+    
     if (min < 10) min = '0' + min;
 
     if (s < 10) s = '0' + s;
@@ -17,6 +20,15 @@ const relogio = setInterval(function time() {
     horas.textContent = hr;
     minutos.textContent = min;
     segundos.textContent = s;
+
+    // configuração da troca de paletas após as 18h
+    let b = body
+
+    if(hr >= 18){
+        b.style.background = 'linear-gradient(120deg, #a9d6e5, #014f86, #012a4a)';
+    }
+    
+    
 
 
 })
